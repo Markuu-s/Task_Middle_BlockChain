53e0bc3fc64c5ffca95ce0ab79ecbc71cae8e28e
Markuu-s <qwert11223381@gmail.com>
2021-09-28 17:53:39 +0300

    [test] Add test for function changeNote.

--- test/AppNotes-test.js
+++ test/AppNotes-test.js
@@ -12,31 +12,57 @@ beforeEach(async () => {
     await appNote.deployed()
 })
 
+let addressNote
+let nameNote
+let surnameNote
+let ageNote
+
+const emptyAddress = '0x0000000000000000000000000000000000000000'
+
+checkData = () => {
+    expect(data['accountAddr']).to.equal(addressNote)
+    expect(data['name']).to.equal(nameNote)
+    expect(data['surname']).to.equal(surnameNote)
+    expect(data['age']).to.equal(ageNote)
+}
+
 describe("AppNote", () => {
     it("Check owner", async () => {
         expect(await appNote.owner()).to.equal(accounts[0].address)
     })
 
     it("Check functions addNote and getNote", async () => {
-        const addressNote = accounts[1].address
-        const nameNote = "Mark"
-        const surnameNote = "Esayan"
-        const ageNote = 18
+        addressNote = accounts[0].address
+        nameNote = "Mark"
+        surnameNote = "Esayan"
+        ageNote = 18
 
         await appNote.connect(accounts[1]).addNote(nameNote, surnameNote, ageNote)
         data = await appNote.connect(accounts[5]).getNote(accounts[1].address)
 
-        checkData = () => {
-            expect(data['accountAddr']).to.equal(addressNote)
-            expect(data['name']).to.equal(nameNote)
-            expect(data['surname']).to.equal(surnameNote)
-            expect(data['age']).to.equal(ageNote)
-        }
         checkData()
 
         data = await appNote.connect(accounts[0]).getNote(accounts[1].address)
-        console.log(data)
         checkData()
     })
     
+    it("Check function changeNote", async () => {
+        addressNote = accounts[7].address
+        nameNote = "Alexey"
+        surnameNote = "Arushkin"
+        ageNote = 24
+
+        await appNote.connect(accounts[1]).addNote(addressNote, nameNote, surnameNote, ageNote)
+        data = await appNote.connect(accounts[1]).getNote(accounts[1].address)
+
+        checkData()
+        ageNote = 40
+        surnameNote = "Yuie"
+    
+        await appNote.connect(accounts[1]).changeNote(emptyAddress, '', surnameNote, ageNote)
+        data = await appNote.connect(accounts[1]).getNote(accounts[1].address)
+        checkData()
+
+    })
+
 })
